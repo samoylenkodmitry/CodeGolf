70e7f6b4bdb4fc2a997bbd3128eab93dd393af4d
dmitrysamoylenko <dmitrysamoylenko@gmail.com>
2017-02-12 17:33:10 +0300

    mathFactCodeNot

--- src/com/samart/mathFactCodeNot.java
+++ src/com/samart/mathFactCodeNot.java
@@ -10,12 +10,10 @@ interface mathFactCodeNot {
         Arrays.stream(
                 new String[]{"0", "1", "0!", "1!", "!0", "!1", "!0!", "!1!", "0!!", "1!!", "!!0", "!!1", "!0!!", "!!!1", "!!!0!!!!", "!!!1!!!!"}
         ).map(
-                s -> new Error() {
-                    int l = s.length(),
-                            n = (l > 1 ? s.split("[01]")[0] : s).length(),
-                            r = l < 2 ? Integer.valueOf(s) :
-                                    n % 2 > 0 == (l - n < 2 && s.charAt(n) < '1') ? 1 : 0;
-                }.r
+                s -> {
+                    int l = s.length(), n = (l > 1 ? s.split("[01]")[0] : s).length();
+                    return l < 2 ? s : (l - n < 2 && s.charAt(n) < '1') == n % 2 > 0 ? 1 : 0;
+                }
         ).forEach(System.out::println);
     }
 }
